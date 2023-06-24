53c6cf203cd94125f54174c70ce241740a6fdcad
Ayush Verma <106621533+AyushUtk@users.noreply.github.com>
2023-06-24 13:16:14 +0530

    icons added

--- content_script.js
+++ content_script.js
@@ -1,10 +1,7 @@
 console.log('Hello World')
-
-var html = "<div id='GPTCHAT' style='background:#fff;position:relative;-webkit-box-sizing:border-box;padding:50px;border-radius:10px;border: 1px solid #35d332;margin-top: 30px;margin-bottom: 30px;color: #205540;margin-left: var(--center-abs-margin); width: 800px;'>Generating ChatGPT Response</div>";
-document.getElementById("appbar").insertAdjacentHTML('afterend', html);
   
 const prompt = "I want to write a novel";
-const apiKey = "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"
+const apiKey = "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"
 const apiUrl =  "https://api.openai.com/v1/completions";
 
 const url = new URL(window.location.href)
@@ -20,14 +17,18 @@ fetch(apiUrl, {
     },
     body: JSON.stringify({
         "model": "text-davinci-003",
-        "prompt": q + "under 100 words",
-        "max_tokens": 200,
+        "prompt": q,
+        "max_tokens": 100,
         "temperature": 0,
     }),
 })
 .then(response => response.json())
 .then(data => {
     const ans = data.choices[0].text;
+
+    var html = "<div id='GPTCHAT' style='background:#fff;position:relative;-webkit-box-sizing:border-box;padding:50px;border-radius:10px;border: 1px solid #35d332;margin-top: 30px;margin-bottom: 30px;color: #205540;margin-left: var(--center-abs-margin); width: 800px;'>Generating ChatGPT Response</div>";
+	document.getElementById("appbar").insertAdjacentHTML('afterend', html);
+
     document.getElementById("GPTCHAT").innerHTML = ans;
 })
 .catch(error => console.error(error));
